4e9b25bf33f1b8eac7e24aa9a9795d3c4dbca14a
MariaESLIW <121664848+MariaESLIW@users.noreply.github.com>
2022-12-30 11:48:08 +0200

    solution

---
+++ Marks/Program.cs
@@ -0,0 +1,38 @@
+﻿int[] marks = new int[10];
+
+marks[0] = 9;
+marks[1] = 7;
+marks[2] = 8;
+marks[3] = 9;
+marks[4] = 5;
+marks[5] = 9;
+marks[6] = 10;
+marks[7] = 3;
+marks[8] = 10;
+marks[9] = 7;
+
+double averageMark = 0.0;
+
+averageMark = (marks[0]+ marks[1] + marks[2] + marks[3] + marks[4] + marks[5] + marks[6] + marks[7] + marks[8] + marks[9]) / 10.0;
+Console.WriteLine("average Mark: " + averageMark);
+
+int maxMark = 0;
+int minMark = 10;
+int summaMarks = 0;
+
+for(int i=0; i<10; i=i+1)
+{
+    if (marks[i]>maxMark)
+    {
+        maxMark = marks[i];
+    }
+    if (marks[i]<minMark)
+    {
+        minMark = marks[i];
+    }
+    summaMarks = summaMarks + marks[i];
+}
+averageMark = summaMarks / 10.0;
+Console.WriteLine("average Mark: " + averageMark);
+Console.WriteLine("min Mark: " + minMark);
+Console.WriteLine("max Mark: " + maxMark);
